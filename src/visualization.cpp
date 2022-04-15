b1364d457029ed2fea3b50de8349c77e94b13e9e
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2022-04-15 02:18:30 -0400

    made viz WAY more featureful

--- src/visualization.cpp
+++ src/visualization.cpp
@@ -1,20 +1,31 @@
 #include "constants.h"
+#include "pcl_conversions/pcl_conversions.h"
 #include <pcl/visualization/cloud_viewer.h>
 
 
 class Visualization {
 
+    struct Viewport {
+        int id = 0;
+    };
+
     struct Cloud {
         enum Flags {
-            COLOR   = 1 << 0,
+            COLOR     = 1 << 0,
+        };
+
+        enum Type {
+            XYZI,
+            XYZL
         };
         
         int flags = 0;
+        Type type = XYZI;
 
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud;
+        sensor_msgs::PointCloud2ConstPtr cloud_msg;
         ros::Subscriber cloud_sub;
         
-        int viewport = 0;
+        std::vector<int> viewports = {0};
         int point_size = 1;
         float opacity = 1.0f;
         union {
@@ -29,82 +40,96 @@ class Visualization {
 
     ros::NodeHandle nh;
 
-    int num_viewports = 1;
+    std::vector<Viewport> viewports;
     std::unordered_map<std::string, Cloud> clouds;
 
     pcl::visualization::PCLVisualizer::Ptr viewer;
-
-    int v1 = 0;
-    int v2 = 0;
 
 public:
     Visualization(int argc, char **argv)
     : viewer(new pcl::visualization::PCLVisualizer("Viewer")) {
 
+        /* Parse Command Line Arguments */
         int cursor = 1;
         while (cursor < argc) {
             std::string s = argv[cursor];
-            if (s == "--cloud") {
-                std::string topic = argv[++cursor];
+            if (s == "--viewports") {
+                const int num_viewports = std::stoi(argv[++cursor]);
+                const float width = 1.0f / num_viewports;
+                for (int i = 0; i < num_viewports; i++) {
+                    Viewport v;
+                    viewer->createViewPort(0 + width * i, 0, 0 + width * (i + 1), 1, v.id) ;
+                    viewer->setBackgroundColor(0.1f * i, 0.1f * i, 0.1f * i, v.id);
+                    viewports.push_back(v);
+                }
+            } else if (s == "--cloud") {
+                cursor += 1;
+                const std::string topic = argv[cursor];
                 auto &cloud = clouds[topic];
                 cloud.cloud_sub = nh.subscribe<sensor_msgs::PointCloud2>(topic, 1, boost::bind(&Visualization::cloud_handler, this, _1, topic));
-                cloud.cloud = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>);
 
-                while (++cursor < argc) {
+                cursor += 1;
+                while (cursor < argc) {
                     s = argv[cursor];
-                    if (s == "--point-size" || s == "-p") {
-                        cloud.point_size = std::stoi(argv[++cursor]);
-                    } else if (s == "--viewport" || s == "-v") {
-                        cloud.viewport = std::stoi(argv[++cursor]);
-                        if (cloud.viewport > 0) {
-                            num_viewports = 2;
+                    if (s == "--type" || s == "-t") {
+                        s = argv[++cursor];
+                        if (s == "0" || s == "intensity") {
+                            cloud.type = Cloud::XYZI;
+                        } else if (s == "1" || s == "label") {
+                            cloud.type = Cloud::XYZL;
                         }
-                    } else if (s == "--color" || s == "-c") {
+                    } else if (s == "-p" || s == "--point-size") {
+                        cloud.point_size = std::stoi(argv[++cursor]);
+                    } else if (s == "-v" || s == "--viewport") {
+                        cloud.viewports.clear();
+                        std::stringstream ss(argv[++cursor]);
+                        while (std::getline(ss, s, ',')) {
+                            cloud.viewports.push_back(std::stoi(s));
+                        }
+                    } else if (s == "-c" || s == "--color") {
                         cloud.flags |= Cloud::COLOR;
+
                         std::stringstream ss(argv[++cursor]);
                         int i = 0;
                         while (std::getline(ss, s, ',')) {
                             cloud.color[i++] = std::stof(s);
                         }
-                    } else if (s == "--opacity" || s == "-o") {
+                    } else if (s == "-o" || s == "--opacity") {
                         cloud.opacity = std::stof(argv[++cursor]);
                     } else {
+                        cursor -= 1;
                         break;
                     }
+                    cursor += 1;
                 }
+                printf("cloud=[topic=[%s], type=[%d], point_size=[%d], color=[%1.1f,%1.1f,%1.1f,], opacity=[%1.2f], viewports=[", 
+                        topic.c_str(), cloud.type, cloud.point_size, cloud.r, cloud.g, cloud.b, cloud.opacity);
+                for (const auto &viewport : cloud.viewports) {
+                    printf("%d,", viewport);
+                }
+                printf("]]\n");
             }
+            cursor += 1;
         }
 
         viewer->initCameraParameters();
         viewer->setShowFPS(false);
 
-        if (num_viewports == 1) {
-            viewer->setBackgroundColor(0, 0, 0);
+        if (viewports.size() == 0) {
+            Viewport v{0};
+            viewports.push_back(v);
+        }
+
+        for (int i = 0; i < viewports.size(); i++) {
+            const auto &viewport = viewports[i];
+            const auto id = std::to_string(viewport.id);
             int y = 10;
             for (const auto &cloud : clouds) {
-                viewer->addText(cloud.first, 10, y, 10, cloud.second.r, cloud.second.g, cloud.second.b, std::to_string(y));
-                y += 14;
-            }
-        } else if (num_viewports == 2) {
-            viewer->createViewPort(0, 0, 0.5, 1, v1);
-            viewer->setBackgroundColor(0, 0, 0, v1);
-
-            int y = 10;
-            for (const auto &cloud : clouds) {
-                if (cloud.second.viewport == 0) {
-                    viewer->addText(cloud.first, 10, y, 10, cloud.second.r, cloud.second.g, cloud.second.b, "1" + std::to_string(y));
-                    y += 14;
-                }
-            }
-
-            viewer->createViewPort(0.5, 0, 1, 1, v2);
-            viewer->setBackgroundColor(0.1, 0.1, 0.1, v2);
-
-            y = 10;
-            for (const auto &cloud : clouds) {
-                if (cloud.second.viewport == 1) {
-                    viewer->addText(cloud.first, 10, y, 10, cloud.second.r, cloud.second.g, cloud.second.b, "2" + std::to_string(y));
-                    y += 14;
+                for (const auto &v : cloud.second.viewports) {
+                    if (v == i) {
+                        viewer->addText(cloud.first, 10, y, 10, cloud.second.r, cloud.second.g, cloud.second.b, id + std::to_string(y), viewport.id);
+                        y += 14;
+                    }
                 }
             }
         }
@@ -116,7 +141,8 @@ public:
 
     void cloud_handler(const sensor_msgs::PointCloud2ConstPtr &cloud_msg, const std::string &topic_name);
 
-    void add_or_update_cloud(const Cloud &cloud, const std::string &cloud_name);
+    void set_cloud_properties(const Cloud &cloud, const std::string &cloud_name, int viewport);
+    void add_or_update_cloud(const Cloud &cloud,  const std::string &cloud_name, int viewport);
 
     bool ok() {
         return !viewer->wasStopped();
@@ -130,33 +156,52 @@ public:
 };
 
 void Visualization::cloud_handler(const sensor_msgs::PointCloud2ConstPtr &cloud_msg, const std::string &topic) {
-    pcl::fromROSMsg(*cloud_msg, *clouds[topic].cloud);
-    add_or_update_cloud(clouds[topic], topic);
+    clouds[topic].cloud_msg = cloud_msg;
+    for (const auto &viewport : clouds[topic].viewports) {
+        add_or_update_cloud(clouds[topic], topic, this->viewports[viewport].id);
+    }
 }
 
-void Visualization::add_or_update_cloud(const Cloud &cloud, const std::string &topic) {
-    pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity(cloud.cloud, "intensity");
-    if (!this->viewer->updatePointCloud<pcl::PointXYZI>(cloud.cloud, intensity, topic)) {
-        int viewport = (cloud.viewport) ? v2 : v1;
-        this->viewer->addPointCloud<pcl::PointXYZI>(cloud.cloud, intensity, topic, viewport);
-        this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, cloud.point_size, topic, viewport);
-        this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, cloud.opacity, topic, viewport);
-        if (cloud.flags & Cloud::COLOR) {
-            this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, cloud.r, cloud.g, cloud.b, topic, viewport);
+void Visualization::set_cloud_properties(const Cloud &cloud, const std::string &id, int viewport) {
+    this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, cloud.point_size, id, viewport);
+    this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, cloud.opacity, id, viewport);
+    if (cloud.flags & Cloud::COLOR) {
+        this->viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, cloud.r, cloud.g, cloud.b, id, viewport);
+    }
+}
+
+void Visualization::add_or_update_cloud(const Cloud &cloud, const std::string &topic, int viewport) {
+    const std::string id = std::to_string(viewport) + topic;
+    switch (cloud.type) {
+        case Cloud::XYZI: {
+            pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+            pcl::fromROSMsg(*cloud.cloud_msg, *cloud_ptr);
+            pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity(cloud_ptr, "intensity");
+            if (!this->viewer->updatePointCloud<pcl::PointXYZI>(cloud_ptr, intensity, id)) {
+                this->viewer->addPointCloud<pcl::PointXYZI>(cloud_ptr, intensity, id, viewport);
+                set_cloud_properties(cloud, id, viewport);
+            }
+            break;
         }
-    }
-}
 
-int loop(Visualization &v) {
-    while(v.ok() && ros::ok()) {
-        ros::spinOnce();
-        v.spin_once(100);
-    }
-    return 0;
+        case Cloud::XYZL: {
+            pcl::PointCloud<pcl::PointXYZL>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZL>);
+            pcl::fromROSMsg(*cloud.cloud_msg, *cloud_ptr);
+            if (!this->viewer->updatePointCloud(cloud_ptr, id)) {
+                this->viewer->addPointCloud(cloud_ptr, id, viewport);
+                set_cloud_properties(cloud, id, viewport);
+            }
+            break;
+        }
+    };
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "visualization"); 
     Visualization v(argc, argv);
-    return loop(v);
+    while(v.ok() && ros::ok()) {
+        ros::spinOnce();
+        v.spin_once(100);
+    }
+    return 0;
 }
